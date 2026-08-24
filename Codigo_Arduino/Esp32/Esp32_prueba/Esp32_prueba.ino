aeda3e5615d433bd42a39829538d78cd5bfd4657
peraltagonzalodavid21 <peraltagonzalodavid21@gmail.com>
2026-08-24 20:23:55 -0300

    devuelta codigo esp32

--- Codigo_Arduino/Esp32/Esp32_prueba/Esp32_prueba.ino
+++ Codigo_Arduino/Esp32/Esp32_prueba/Esp32_prueba.ino
@@ -1,8 +1,8 @@
 // ==============================================================================
 // PROYECTO: Control de Carroza - PRUEBA CON ESP32 (Bluetooth Integrado)
 // ==============================================================================
-// IMPORTANTE: El ESP32 ya tiene Bluetooth clásico integrado, así que NO hace 
-// falta conectar el módulo HC-05. Simplemente vinculá el celular al 
+// IMPORTANTE: El ESP32 ya tiene Bluetooth clásico integrado, así que NO hace
+// falta conectar el módulo HC-05. Simplemente vinculá el celular al
 // dispositivo llamado "Carroza_ESP32" y la app va a funcionar igual.
 // ==============================================================================
 
@@ -16,11 +16,13 @@
 BluetoothSerial SerialBT;
 
 // Array con pines seguros para usar como salida en el ESP32
-// (Evitamos pines de booteo como el 0, 2, 5, 12 y 15 para evitar problemas al encender)
+// (Evitamos pines de booteo como el 0, 2, 5, 12 y 15 para evitar problemas al
+// encender)
 int pinesRelevadores[] = {13, 14, 25, 26, 27, 32, 33, 16, 17, 18, 19, 21};
 
 void setup() {
-  // Iniciamos el puerto Serial normal para ver mensajes en la compu (monitor serie)
+  // Iniciamos el puerto Serial normal para ver mensajes en la compu (monitor
+  // serie)
   Serial.begin(115200);
 
   // Configuramos todos los pines de los relés como SALIDA y los apagamos
@@ -30,58 +32,96 @@ void setup() {
   }
 
   // Iniciamos el Bluetooth con el nombre que va a aparecer en el celular
-  SerialBT.begin("Carroza_ESP32"); 
-  Serial.println("¡El Bluetooth del ESP32 está listo! Vinculalo desde el celular.");
+  SerialBT.begin("Carroza_ESP32");
+  Serial.println(
+      "¡El Bluetooth del ESP32 está listo! Vinculalo desde el celular.");
 }
 
 void loop() {
   // Comprobamos si nos llega algún comando desde la App por el Bluetooth
   if (SerialBT.available() > 0) {
     char comando = SerialBT.read();
-    
-    // Imprimimos la letra recibida en la PC para que veas que está llegando bien
+
+    // Imprimimos la letra recibida en la PC para que veas que está llegando
+    // bien
     Serial.print("Recibido: ");
     Serial.println(comando);
 
     // =============== MOVIMIENTO (Botones Momentáneos) ===============
     // Adelante
-    if (comando == 'A') { rele1_ON(); }
-    else if (comando == 'a') { rele1_OFF(); }
+    if (comando == 'A') {
+      rele1_ON();
+    } else if (comando == 'a') {
+      rele1_OFF();
+    }
     // Atrás
-    else if (comando == 'B') { rele2_ON(); }
-    else if (comando == 'b') { rele2_OFF(); }
+    else if (comando == 'B') {
+      rele2_ON();
+    } else if (comando == 'b') {
+      rele2_OFF();
+    }
     // Izquierda
-    else if (comando == 'C') { rele3_ON(); }
-    else if (comando == 'c') { rele3_OFF(); }
+    else if (comando == 'C') {
+      rele3_ON();
+    } else if (comando == 'c') {
+      rele3_OFF();
+    }
     // Derecha
-    else if (comando == 'D') { rele4_ON(); }
-    else if (comando == 'd') { rele4_OFF(); }
+    else if (comando == 'D') {
+      rele4_ON();
+    } else if (comando == 'd') {
+      rele4_OFF();
+    }
 
     // =============== ACCESORIOS / LUCES (Botones Toggle) ===============
     // Mas
-    else if (comando == 'E') { rele5_ON(); }
-    else if (comando == 'e') { rele5_OFF(); }
+    else if (comando == 'E') {
+      rele5_ON();
+    } else if (comando == 'e') {
+      rele5_OFF();
+    }
     // Menos
-    else if (comando == 'F') { rele6_ON(); }
-    else if (comando == 'f') { rele6_OFF(); }
+    else if (comando == 'F') {
+      rele6_ON();
+    } else if (comando == 'f') {
+      rele6_OFF();
+    }
     // SW1
-    else if (comando == 'G') { rele7_ON(); }
-    else if (comando == 'g') { rele7_OFF(); }
+    else if (comando == 'G') {
+      rele7_ON();
+    } else if (comando == 'g') {
+      rele7_OFF();
+    }
     // SW2
-    else if (comando == 'H') { rele8_ON(); }
-    else if (comando == 'h') { rele8_OFF(); }
+    else if (comando == 'H') {
+      rele8_ON();
+    } else if (comando == 'h') {
+      rele8_OFF();
+    }
     // SW3
-    else if (comando == 'I') { rele9_ON(); }
-    else if (comando == 'i') { rele9_OFF(); }
+    else if (comando == 'I') {
+      rele9_ON();
+    } else if (comando == 'i') {
+      rele9_OFF();
+    }
     // SW4
-    else if (comando == 'J') { rele10_ON(); }
-    else if (comando == 'j') { rele10_OFF(); }
+    else if (comando == 'J') {
+      rele10_ON();
+    } else if (comando == 'j') {
+      rele10_OFF();
+    }
     // SW5
-    else if (comando == 'K') { rele11_ON(); }
-    else if (comando == 'k') { rele11_OFF(); }
+    else if (comando == 'K') {
+      rele11_ON();
+    } else if (comando == 'k') {
+      rele11_OFF();
+    }
     // SW6
-    else if (comando == 'L') { rele12_ON(); }
-    else if (comando == 'l') { rele12_OFF(); }
+    else if (comando == 'L') {
+      rele12_ON();
+    } else if (comando == 'l') {
+      rele12_OFF();
+    }
 
     // =============== EMERGENCIA ===============
     else if (comando == 'X') {
@@ -94,7 +134,7 @@ void loop() {
 // FUNCIÓN BASE: Controla un relé específico
 // ==============================================================================
 void controlarRele(int id, bool estado) {
-  int indice = id - 1; 
+  int indice = id - 1;
   if (indice >= 0 && indice < 12) {
     digitalWrite(pinesRelevadores[indice], estado ? LOW : HIGH);
   }
@@ -109,7 +149,7 @@ void apagarTodo() {
 }
 
 // ==============================================================================
-// FUNCIONES INDIVIDUALES 
+// FUNCIONES INDIVIDUALES
 // ==============================================================================
 void rele1_ON() { controlarRele(1, true); }
 void rele1_OFF() { controlarRele(1, false); }
